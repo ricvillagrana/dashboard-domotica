b2d93120f49224902f298089522a718f63358990
Ricardo Villagrana <rvillagrana@protonmail.com>
2018-05-29 16:34:15 -0500

    [IMP] Add Logs Models and migrations

---
+++ database/migrations/2018_05_29_213227_create_nigth_movement_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNigthMovementLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nigth_movement_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nigth_movement_logs');
+    }
+}
